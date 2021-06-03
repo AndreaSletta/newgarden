305525ffd87a1a5e6d0860b744d6651778567f64
AndreaSletta <70203798+AndreaSletta@users.noreply.github.com>
2021-06-03 14:30:38 +0200

    scroll test on carousel

--- script/fetchapi.js
+++ script/fetchapi.js
@@ -118,6 +118,9 @@ async function getAllPosts() {
         }
       }
     };
+    carousel.addEventListener("scroll", forLeftButton);
+
+    carousel.addEventListener("scroll", forRightButton);
 
     var style = window.getComputedStyle(carousel);
   } catch (error) {
